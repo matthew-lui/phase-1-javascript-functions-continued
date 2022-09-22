4cdfd8ecc5ea9de7b978efda6ea6af44a513c9a8
matthew-lui <107003143+matthew-lui@users.noreply.github.com>
2022-09-22 10:47:51 -0400

    commit tree 9a089ba6925cc5edb9c8812ee5d66a781e3491bc

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return (`This Saturday, I want to ${activity}!`)
+}
+saturdayFun();
+saturdayFun('bathe my dog');
+
+function mondayWork(activity = 'go to the office'){
+    return (`This Monday, I will ${activity}.`)
+}
+
+mondayWork();
+mondayWork('work from home');
+
+function wrapAdjective(outer, inner){
+
+}
+
+
+function wrapAdjective(adjective = "special") {
+    const wrap = function (star = "*", wrapStick = "||") {
+    return `You are, ${star}, ${wrapStick}, ${adjective}, ${wrapStick} ${star}!`
+    };
+    return wrapAdjective;
+};
+wrapAdjective();
